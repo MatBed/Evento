4318695d1d7b844ab685a66efd8544608462fcd6
MatBed <mateuszbedla@gmail.com>
2020-01-26 12:29:42 +0100

    Create projects, create domain and repo interfaces

---
+++ Evento.Core/Domain/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evento.Core.Domain
+{
+    public class Ticket : Entity
+    {
+        public Guid EventId { get; set; }
+        public int Seating { get; set; }
+        public decimal Price { get; set; }
+        public Guid? UserId { get; set; }
+        public string UserName { get; set; }
+        public DateTime? PurchasedAt { get; set; }
+        public bool Purchased => UserId.HasValue;
+
+        protected Ticket() { }
+
+        public Ticket(Event @event, int seating, decimal price)
+        {
+            EventId = @event.Id;
+            Seating = seating;
+            Price = price;
+        }
+    }
+}
